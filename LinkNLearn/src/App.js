53f7b5af4081d79762b8807edc73493dd73d5d61
Alexandre Santos <sasle57@gmail.com>
2021-07-06 23:49:39 -0300

    Creating react app with devextreme

---
+++ LinkNLearn/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'devextreme/dist/css/dx.light.css';
+import Button from 'devextreme-react/button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>Olá!</p>
+      <Button
+        text="Botão do devextreme"
+      />
+    </div>
+  );
+}
+
+export default App;
