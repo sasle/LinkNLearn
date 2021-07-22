377b2222023411ae12e500201f7d1c83f3b35955
Alexandre Santos <sasle57@gmail.com>
2021-07-22 19:23:21 -0300

    adding first routes

--- LinkNLearn/src/App.js
+++ LinkNLearn/src/App.js
@@ -1,17 +1,29 @@
-import './App.css';
-import 'devextreme/dist/css/dx.light.css';
-import Button from 'devextreme-react/button';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
-
-function App() {
-  return (
-    <div className="App">
-      <p>Olá!</p>
-      <Button
-        text="Botão do devextreme"
-      />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- LinkNLearn/src/pages/About/index.js
+++ LinkNLearn/src/pages/About/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class Home extends Component {
+class About extends Component {
   render() {
     return (
         <div>
-          <h2>Home</h2>
+          <h2>About Us</h2>
         </div>
     );
   }
 }
 
-export default Home;
+export default About;
