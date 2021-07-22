d3c68016ad849039e43062927db8b7096593a83c
Alexandre Santos <sasle57@gmail.com>
2021-07-22 20:22:42 -0300

    material icons

--- LinkNLearn/src/components/Header/index.js
+++ LinkNLearn/src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
 
 function Header() {
   return (
     <header className="header">
       <Link to={'/'}>
-        <Button color="primary">Home</Button>
+        <Button color="primary"><HomeIcon />Home</Button>
       </Link>
       <Link to={'/about'}>
         <Button>About Us</Button>
