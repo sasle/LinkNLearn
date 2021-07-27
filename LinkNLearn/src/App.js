eea527e244b4a10205b394a4f70ff2cd85735d36
Alexandre Santos <sasle57@gmail.com>
2021-07-26 21:03:13 -0300

    header component

--- LinkNLearn/src/App.js
+++ LinkNLearn/src/App.js
@@ -1,20 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import './assets/custom.css';
 
-class App extends Component {
-  render() {
-    return (
+
+function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#3577CC',
+      },
+      secondary: {
+        main: '#fff',
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
         </Switch>
       </Router>
-    );
-  }
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- LinkNLearn/src/components/Header/index.js
+++ LinkNLearn/src/components/Header/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
+import Logo from '../../assets/images/logo.png';
+import './style.css';
+
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Search from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Button, Grid } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField'
+
 
 function Header() {
   return (
     <header className="header">
-      <Link to={'/'}>
-        <Button variant="contained" color="primary"><HomeIcon />Home</Button>
-      </Link>
-      <Link to={'/about'}>
-        <Button>About Us</Button>
-      </Link>
+      <Grid container alignItems="center" justifyContent="space-between">
+        <Grid item container xs={12} sm={4} md={2} alignItems="flex-start">
+          <img src={Logo} alt="Link & Learn logo" />
+        </Grid>
+        <Grid item xs={false} sm={4} md={5}>
+          <TextField
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+              placeholder: 'O que você procura?',
+            }}
+            className="searchBar"
+          />
+        </Grid>
+        <Grid item container md={3} spacing={5} justifyContent="flex-end">
+          <Grid item md={"auto"}>
+            <Link>
+              <p>Veja todos os cursos</p>
+            </Link>
+          </Grid>
+          <Grid item md={"auto"}>
+            <Link>
+              <ShoppingCartIcon />
+            </Link>
+          </Grid>
+          <Grid item md={"auto"}>
+            <span style={{ display: 'flex' }}>
+              <Link>
+                <p>Aluno</p>
+              </Link>
+              <p style={{ padding: '0 .3em' }}>|</p>
+              <Link>
+                <p>Professor</p>
+              </Link>
+            </span>
+          </Grid>
+
+        </Grid>
+        <Grid item xs={false} sm={4} md={2}>
+          <Button color="primary" variant="contained" className="button">Login</Button>
+        </Grid>
+      </Grid>
     </header>
   );
 }
